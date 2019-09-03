8bef82d6c9cf5c4d10fc22069686d6fc308269f6
klimbim <klimcho@mail.ru>
2019-09-03 17:54:38 +0300

    slider index html
    поправил адаптив

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'img/',
-              publicPath: '/img/'
+              publicPath: './img/'
             }
           }
         ]
